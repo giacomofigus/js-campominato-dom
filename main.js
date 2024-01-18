4af69ef0fe0d90a04186335eedfd3dca8a0e67cb
MyUsername <figusgiacomo04@gmail.com>
2024-01-18 10:40:00 +0100

    fix: punteggio

--- main.js
+++ main.js
@@ -2,14 +2,18 @@
 let btnHtml = document.getElementById("button");
 let grigliaHtml = document.getElementById("grid");
 const selectHtml = document.getElementById("menu");
+let punteggioHtml = document.getElementById("punteggio")
 
 let arrayNumb = [];
-let giocoInCorso = true
+let giocoInCorso = true;
 
-
+// al click del bottone
 btnHtml.addEventListener('click', function(){
     grigliaHtml.style.setProperty("display", "flex")
 
+    let punteggio = 0;
+
+    // così che le griglie non si sommino al click
     grigliaHtml.innerHTML = ''
 
 
@@ -24,40 +28,63 @@ btnHtml.addEventListener('click', function(){
 
     // creo i box
     for(let i = 1; i <= selectHtml.value; i++) {
+
+        //Creo il div box
         let box = document.createElement("div")
+
+        // Gli do la proprietà  in modo che la griglia cambi in base alla difficoltà
         box.style.setProperty("flex-basis", `calc(100% / ${Math.sqrt(selectHtml.value)})`)
+
+        // inserisco nella box il numero 
         box.innerHTML = `<span>${i}</span>`
+
+        //gli aggiungo la classe css 
         box.classList.add("box-easy")
 
+        // AL CLICK DEL BOX
         box.addEventListener('click', function(){
+
+            //se la variabile booleana è true
             if (giocoInCorso){
+
+                // al click appare il numero nella box
                 let spanHtml = parseInt(this.querySelector("span").innerText);
 
-                console.log(parseInt(selectHtml.value));
-
+                //se l'array contiene un numero nello span
                 if (arrayNumb.includes(spanHtml)) {
+                    //al click della box rossa il gioco termina
                     this.classList.add("rosso");
                     giocoInCorso = false;
                     alert("Hai perso")
+
+                    //mostra il punteggio
+                    punteggioHtml.innerHTML = `Punteggio: ${punteggio}`
+
                 } else{
+                    //se clicca blu continua
                     this.classList.add("blue")
 
                     const celleBlu = document.querySelectorAll('.box-easy.blue')
 
-                    console.log(celleBlu.length);
-
+                    // se il numero di celle blu è uguale al valore della select - numero degli elementi nell'array hai vinto
                     if(celleBlu.length == selectHtml.value - arrayNumb.length){
                         giocoInCorso = false;
                         alert("Hai vinto")
                     }
+
+                    //il punteggio aumenta ad ogni click della cella
+                    punteggio++
+
                 }
+
+                
                 console.log(`Hai cliccato il numero: ${spanHtml}`);
             } 
 
         })
 
         
-
+        //INSERISCI BOX
         grigliaHtml.appendChild(box);
     }
 })
